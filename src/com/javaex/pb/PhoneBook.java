e59757e699992fba7c448368be1fe83344d2f23d
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-13 17:24:18 +0900

    second

--- src/com/javaex/pb/PhoneBook.java
+++ src/com/javaex/pb/PhoneBook.java
@@ -1,5 +1,16 @@
 package com.javaex.pb;
 
 public class PhoneBook {
+	private String name;
+	private String hp;
+	private String company;
+	
+	public PhoneBook(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+	
+	
 
 }
